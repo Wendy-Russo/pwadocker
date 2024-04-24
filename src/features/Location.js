72ed5575c22210d1416f81137a34ce66ba7e244f
jules <boismond.jules@gmail.com>
2024-04-24 11:02:38 +0200

    stockage location on cookie

--- src/features/Location.js
+++ src/features/Location.js
@@ -3,16 +3,24 @@ import { MapContainer, TileLayer, Marker, Polyline, useMapEvents } from 'react-l
 import L from 'leaflet';
 import 'leaflet/dist/leaflet.css';
 import markerIconPng from "../images/marker.png";
-import {Icon} from 'leaflet';
+import { Icon } from 'leaflet';
 
 function LocationMarker({ positions, setPositions }) {
   const map = useMapEvents({
     locationfound(e) {
       const newPosition = e.latlng;
-      console.log('Location found:', e.latlng);
-      setPositions(prevPositions => [...prevPositions, newPosition]);
+      console.log('Location found:', newPosition);
+
+      setPositions(prevPositions => {
+        const newPositions = [...prevPositions, newPosition];
+        const expiryDate = new Date();
+        expiryDate.setDate(expiryDate.getDate() + 7);  // Le cookie expire dans 7 jours
+        document.cookie = `userPositions=${JSON.stringify(newPositions)}; expires=${expiryDate.toUTCString()}; path=/`;
+
+        return newPositions;
+      });
+
       if (map) map.flyTo(newPosition, map.getZoom());
-      console.log('Location found:', e.latlng);
     },
     locationerror(e) {
       console.error('Location error:', e.message);
@@ -22,137 +30,59 @@ function LocationMarker({ positions, setPositions }) {
   useEffect(() => {
     const interval = setInterval(() => {
       if (map) map.locate();
-      console.log('Location updated');
     }, 5000); // MAJ location toutes les 5 secondes
 
-    return () => clearInterval(interval); 
+    return () => clearInterval(interval);
   }, [map]);
 
   return null;
 }
 
 function MapComponent() {
-  const [positions, setPositions] = useState([
-    [51.505, -0.09],  // London
-    [48.8566, 2.3522] // Paris
-  ]);
-  const [map, setMap] = useState(null);
+  const [positions, setPositions] = useState([]);
 
-  // useEffect(() => {
-  // }, [map, positions]);
+  useEffect(() => {
+    const cookieData = document.cookie.split('; ').find(row => row.startsWith('userPositions='));
+    if (cookieData) {
+      const decodedCookie = decodeURIComponent(cookieData.split('=')[1]);
+      const savedPositions = JSON.parse(decodedCookie);
+      setPositions(savedPositions);
+    }
+  }, []);
 
   return (
     <>
-    <div style={{
-      display: 'grid', 
-      justifyContent: 'center', 
-      alignItems: 'center',
-      height:'100%',
-      width:'100%'
-    }}>
-      <MapContainer
-        center={{ lat: 50.1109, lng: 0.1313 }}
-        zoom={6}
-        scrollWheelZoom={false}
-        style={{ 
-          height: '100%', 
-          width: '100vh',
-          filter:'brightness(0.75) contrast(1.5)'
-        }}
-        whenCreated={setMap}
-        
-      >
-        <TileLayer
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        />
-        <LocationMarker positions={positions} setPositions={setPositions} />
-        {positions.map((position, idx) => (
-          <Marker key={idx} position={position} icon={new Icon({iconUrl: markerIconPng, iconSize: [40, 40], iconAnchor: [19, 35]})}/>
-        ))}
-        <Polyline positions={positions} color="blue" />
-      </MapContainer>
-    </div>
+      <div style={{
+        display: 'grid', 
+        justifyContent: 'center', 
+        alignItems: 'center',
+        height: '100%',
+        width: '100%'
+      }}>
+        <MapContainer
+          center={{ lat: 50.1109, lng: 0.1313 }}
+          zoom={6}
+          scrollWheelZoom={false}
+          style={{ 
+            height: '100%', 
+            width: '100vh',
+            filter: 'brightness(0.75) contrast(1.5)'
+          }}
+          
+        >
+          <TileLayer
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+          <LocationMarker positions={positions} setPositions={setPositions} />
+          {positions.map((position, idx) => (
+            <Marker key={idx} position={position} icon={new Icon({iconUrl: markerIconPng, iconSize: [40, 40], iconAnchor: [19, 35]})}/>
+          ))}
+          <Polyline positions={positions} color="blue" />
+        </MapContainer>
+      </div>
     </>
   );
 }
 
 export default MapComponent;
-
-
-
-// import React, { useState, useEffect } from 'react';
-// import { MapContainer, TileLayer, Marker, useMapEvents, Polyline } from 'react-leaflet';
-// import L from 'leaflet';
-// import 'leaflet/dist/leaflet.css';
-// import 'leaflet-routing-machine';
-
-// function LocationMarker({ positions, setPositions }) {
-//   const map = useMapEvents({
-//     locationfound(e) {
-//       const newPosition = e.latlng;
-//       setPositions(prevPositions => [...prevPositions, newPosition]);
-//       if (map) map.flyTo(newPosition, map.getZoom());
-//       console.log('Location found:', e.latlng);
-//     }
-//   });
-
-//   useEffect(() => {
-//     const interval = setInterval(() => {
-//       if (map) map.locate();
-//     }, 10000); // Update location every 10 seconds
-
-//     return () => clearInterval(interval); // Cleanup interval on component unmount
-//   }, [map]);
-
-//   return null; // Since the markers will be rendered by MapComponent, we don't need to return anything here.
-// }
-
-// function MapComponent() {
-//   const [positions, setPositions] = useState([
-//     [51.505, -0.09],  // London
-//     [48.8566, 2.3522] // Paris
-//   ]);
-//   const [map, setMap] = useState(null);
-
-//   useEffect(() => {
-//     if (map && positions.length > 1) {
-//       const routingControl = L.Routing.control({
-//         waypoints: positions.map(pos => L.latLng(pos)),
-//         routeWhileDragging: true,
-//         addWaypoints: false,
-//         draggableWaypoints: false,
-//         fitSelectedRoutes: true,
-//         showAlternatives: false
-//       }).addTo(map);
-
-//       return () => {
-//         if (map) map.removeControl(routingControl);
-//       };
-//     }
-//   }, [map, positions]);
-
-//   return (
-//     <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '50vh', marginTop: "20px" }}>
-//       <MapContainer
-//         center={{ lat: 50.1109, lng: 0.1313 }} // Roughly centered between London and Paris
-//         zoom={6}
-//         scrollWheelZoom={false}
-//         style={{ height: '50vh', width: '50vh' }}
-//         whenCreated={setMap}
-//       >
-//         <TileLayer
-//           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-//           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-//         />
-//         <LocationMarker positions={positions} setPositions={setPositions} />
-//         {positions.map((position, idx) => (
-//           <Marker key={idx} position={position}>
-//           </Marker>
-//         ))}
-//       </MapContainer>
-//     </div>
-//   );
-// }
-
-// export default MapComponent;
